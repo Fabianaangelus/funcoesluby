4662b8eb693d68f640fa2582de97197f11f50b1b
Fabiana Gomes <fabianaaangelus@gmail.com>
2022-02-23 18:55:29 -0300

    exercicio fatorial

---
+++ ExercicioFatorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ExercicioFatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            {
+                Console.WriteLine("Digite um valor maior que 1...");
+                double resultado = CalcularFatorial(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine($"Valor fatorial é: {resultado.ToString()}");
+                Console.ReadKey();
+
+            }
+        }
+
+        private static double CalcularFatorial(int numero)
+        {
+            double resultado = 1;
+            while (numero != 1)
+            {
+                resultado = resultado * numero;
+                numero = numero - 1;
+            }
+            return resultado;
+        }
+    }
+}
+    
+
